504d8a373b97c574a33ce78e6d29bf22361d8f1d
Masum <jim.masum@gmail.com>
2017-12-13 02:39:10 +0600

    Initial commit

---
+++ app/src/main/java/com/masumjim/monnan/MainActivity.java
@@ -0,0 +1,22 @@
+package com.masumjim.monnan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://monnan-fashion.com");
+        myWebView.setWebViewClient(new WebViewClient());
+
+    }
+}
